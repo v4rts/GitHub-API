3de9e670afc920a1b209c31c993e1cffb8ee0416
v4rts <v4rts@yandex.ru>
2020-10-06 19:36:39 +0300

    json received

--- gitapi/src/comp/Search.jsx
+++ gitapi/src/comp/Search.jsx
@@ -5,13 +5,21 @@ let Search = () => {
     const [data, setData] = useState({});
     const [username, setUsername] = useState("");
 
-    let changeHandler = (e) => {
-        setUsername(e.target.value);
+    let changeHandler = (input) => {
+        setUsername(input.target.value);
+    };
+
+    let submitHandler = async e => {
+        e.preventDefault();
+        const profile = await fetch(`https://api.github.com/users/${username}`);
+        const json = await profile.json();
+        console.log(json);
     };
 
     return(
         <div>
             <input type = "text" value = {username} onChange = {changeHandler} />
+            <button type = "submit"  onClick = {submitHandler}>Search!</button>
         </div>
     )
 }
