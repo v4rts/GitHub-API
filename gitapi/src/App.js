9df1741b24fb0d7af3fccbc64c18495040a18415
v4rts <v4rts@yandex.ru>
2021-01-12 19:56:03 +0500

    styles recast

--- gitapi/src/App.js
+++ gitapi/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Search from "./comp/Search";
+import styles from "./styles/Search.module.css"
 
 class App extends React.Component {
     render() {
         return (
-            <div className="Search__Wrapper">
+            <div className={styles.appwrapper}>
                 <Search />
             </div>
         )
